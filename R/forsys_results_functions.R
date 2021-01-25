727e5131a7e69ac9027e1b474464836a8568411f
rlank <robblankston@gmail.com>
2021-01-25 14:32:34 -0800

    Pushing some changes to UI and updates to Dockerfile before switching over and updating output writing to use subdirectories

--- R/forsys_results_functions.R
+++ R/forsys_results_functions.R
@@ -1,9 +1,18 @@
 
-read_results_file <- function(path) {
-  # TODO check for file type, but probably will be CSV
-	readr::read_csv(path)
+# read_results_file <- function(path) {
+#   # TODO check for file type, but probably will be CSV
+# 	readr::read_csv(path)
+# }
+
+cumulate_results <- function(results_data) {
+	results_data %>%
+		mutate()
 }
 
-attainment_chart <- function(results_data) {
+attainment_chart_by_target_treated <- function(results_data, target_field, priority) {
 
+}
+
+production_frontiers_chart <- function(results_data) {
+	
 }
