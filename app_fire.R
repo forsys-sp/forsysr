9c9cddd4b64c75f58a3e0629ba763e079311dded
Houtmanr <Houtmanr@FOCUS.forestry.oregonstate.edu>
2021-06-29 09:43:47 -0700

    Added new annual target to app_fire.R.

--- app_fire.R
+++ app_fire.R
@@ -1,3 +1,4 @@
+setwd('C:/Users/houtmanr/ForSysRepo/')
 library(shiny)
 
 source('R/forsys_libraries.R')
@@ -23,8 +24,7 @@ source('server.R')
 
 source('R/fire_misc_func.R')
 
-input_stand_fire_intersect <- 'data/hexnet_west_fsim19_30reps_intersect.csv'
-input_stand_fire_intersect <- '../../Dropbox/!!projects/aa_10yr_uncertainity/data/west_usfs_hexnet_fsim19_omernik6x_deciles5x_futures20x_intersect.csv';
+input_stand_fire_intersect <- 'data/west_usfs_hexnet_fsim19_omernik6x_deciles5x_futures20x_intersect.csv'
 
 input_stand <- 'data/hexnet_west_fs.csv'
 
@@ -36,25 +36,30 @@ if(exists('hex') == FALSE) hex <- fread(input_stand)
 planning_years = 20
 
 # 10-year ramp-up
-annual_project_target = logisticFunc(1:10, start= 0, end=1.228e6)
+#annual_project_target = logisticFunc(1:10, start= 0, end=1.228e6)
+
+# 10-year ramp (6.6 M ha treated)
+max_rx_rate = 1200000 # maximum 1.2 million ha per year
+annual_project_target = max_rx_rate * logisticFunc(yr = 1:10, mid = 5, normalize = T)
 
 # 20-year plan w/ 10-year ramp-up
-annual_project_target = logisticFunc(seq(1,10,length.out=10), start=0, end=4.332e5); annual_project_target[11:20] <- 4.332e5
+#annual_project_target = logisticFunc(seq(1,10,length.out=10), start=0, end=4.332e5); annual_project_target[11:20] <- 4.332e5
 
 #########################################
 ## Run FORSYS W/ FIRE                  ##
 #########################################
 
-run('config_TenYearPlan_WW_FS.R', fire_dynamic_forsys = TRUE, fire_intersect_table = f_df %>% filter(DECILE == 5, FUTURE == 1))
+run(config_file = 'config_TenYearPlan_WW_FS.R', fire_dynamic_forsys = TRUE, fire_random_projects = FALSE, fire_intersect_table = f_df %>% filter(DECILE == 5, FUTURE == 1))
 
 # run multiple scenarios using purrr ...
 
-fire_scn_inputs = expand.grid(DECILE = 9, FUTURE = 1:10)
+fire_scn_inputs = expand.grid(DECILE = 5:9, FUTURE = 1:2)
 
 fire_scn_inputs %>% pmap(function(...){
   scn_i = tibble(...)
   fires_i <- fread(input_stand_fire_intersect) %>% right_join(scn_i, by = names(scn_i)) %>% dplyr::select(-n, -PA_ID, -OWNER, -AREA_HA)
-  run(config_file = 'config_TenYearPlan_WW_FS.R', write_tags='_Dyn2', dynamic_forsys = TRUE, fire_intersect_table = fires_i, ...)
+  run(config_file = 'config_TenYearPlan_WW_FS.R', write_tags='_Dyn4', fire_dynamic_forsys = TRUE, fire_random_projects = FALSE,
+      fire_intersect_table = fires_i, ...)
 })
 
 #########################################
@@ -65,10 +70,10 @@ fire_scn_inputs %>% pmap(function(...){
 process_outputs('config_TenYearPlan_WW_FS.R',
                 project_tag = 'proj',
                 stand_tag = 'stnd',
-                write_tags='_Dyn2',
+                write_tags='',
                 fires_dat = fread(input_stand_fire_intersect) %>%
                   dplyr::select(-n, -PA_ID, -OWNER, -AREA_HA) %>%
-                  filter(DECILE == 9, FUTURE %in% c(1:3)))
+                  filter(FUTURE %in% c(1:10)))
 
 #########################################
 ## Animate FORSYS W/ FIRE outputs     ##
@@ -76,7 +81,7 @@ process_outputs('config_TenYearPlan_WW_FS.R',
 
 # see R/fire_misc_animations.R
 
-tmp <- fread('output/WW_10yr_FS_80p_Dyn/_stnd_Dyn_combined.csv')
+tmp <- fread('output/WW_10yr_FS_80p/_stnd_combined.csv')
 tmp %>% group_by(DECILE, FUTURE) %>% summarize(AREA_HA = sum(AREA_HA, na.rm=T))
 
 
